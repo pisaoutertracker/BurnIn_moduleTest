13bec09ec48cf78fc6977d9fba4af225771f31f7
silviodonato <silvio.donato@cern.ch>
2023-12-14 17:49:27 +0100

    add Nevents

--- PS_Module_settings.py
+++ PS_Module_settings.py
@@ -1,18 +1,22 @@
 ### Configuration one-shot ###
 config = {
     "commonSettings" : "pippo",
+    "Nevents" : "100",
     "boards" : {
         "0": {
-            "ip" : "fc7ot3:50001",
+            "ip" : "fc7ot2:50001",
             "opticalGroups" : {
                 "0" : {
                     "lpGBT" : "lpGBT_v1.txt",
                     "hybrids" : {
                         "0" : {
+#                            "strips" : [0],
+#                            "pixels" : [8],
                             "strips" : [0, 1, 2, 3, 4, 5, 6, 7],
                             "pixels" : [8, 9, 10, 11, 12, 13, 14, 15],
                         },
                         "1" : {
+#			     "strips" : [0]
                             "strips" : [0, 1, 2, 3, 4, 5, 6, 7],
                             "pixels" : [8, 9, 10, 11, 12, 13, 14, 15],
                         },
