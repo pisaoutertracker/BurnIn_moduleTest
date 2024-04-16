36f189754b3b608436db20a59e88960d26f25718
arizzi <Andrea.Rizzi@cern.ch>
2024-04-16 09:39:58 +0200

    use gitlab instead of github

--- buildNewDockerImage/build.sh
+++ buildNewDockerImage/build.sh
@@ -1,5 +1,5 @@
 export VERSION="v4-23"
 docker login gitlab-registry.cern.ch
-docker build -t docker.io/sdonato/pisa_module_test:ph2_acf_$VERSION --build-arg "VERSION=$VERSION" .
+docker build -t gitlab-registry.cern.ch/cms-pisa/pisatracker/pisa_module_test:ph2_acf_$VERSION --build-arg "VERSION=$VERSION" .
 docker login
-docker push docker.io/sdonato/pisa_module_test:ph2_acf_$VERSION
+docker push gitlab-registry.cern.ch/cms-pisa/pisatracker/pisa_module_test:ph2_acf_$VERSION
