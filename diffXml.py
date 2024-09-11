cdfdeaf426869c14f8471931ef1f762971771094
cmspisa <silvio.donato@cern.ch>
2024-09-11 15:30:26 +0200

    compare PS_Module_v2p1.xml, ModuleTest_settings.xml in diffXml

--- diffXml.py
+++ diffXml.py
@@ -1,4 +1,4 @@
 from xmldiff import main
-diff = main.diff_files("ot3.xml", "ModuleTest_settings.xml")
+diff = main.diff_files("PS_Module_v2p1.xml", "ModuleTest_settings.xml")
 for a in diff:
    print(a)
