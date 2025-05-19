dbf63e55d6090fabaace0911c52cde0ae724690d
cmspisa <silvio.donato@cern.ch>
2025-05-19 18:13:33 +0200

    migration to v6-10

--- moduleTest.py
+++ moduleTest.py
@@ -2,7 +2,7 @@
 
 ### Default values 
 verbose = 0
-lastPh2ACFversion = "ph2_acf_v6-08"
+lastPh2ACFversion = "ph2_acf_v6-10"
 xmlPyConfigFile = "PS_Module_settings.py"
 ip="192.168.0.45"
 port=5000
@@ -10,8 +10,8 @@ xmlOutput="ModuleTest_settings.xml"
 defaultCommand="PSquickTest"
 ##xmlTemplate="PS_Module_template.xml"
 xmlTemplate="PS_Module_v2p1.xml"
-firmware_5G="ps8m5gcic2l12octal8dio5v301" #"ps8m5gcic2l12octal8dio5tluv300" ##5 GBps - https://udtc-ot-firmware.web.cern.ch/?dir=v3-00/ps_8m_5g_cic2_l12octa_l8dio5_tlu
-firmware_10G="ps8m10gcic2l12octal8dio5v301" ##10 GBps - https://udtc-ot-firmware.web.cern.ch/?dir=v3-00/ps_8m_10g_cic2_l12octa_l8dio5_tlu
+firmware_5G="ps8m5gcic2l12octal8dio5v303" #"ps8m5gcic2l12octal8dio5tluv300" ##5 GBps - https://udtc-ot-firmware.web.cern.ch/?dir=v3-00/ps_8m_5g_cic2_l12octa_l8dio5_tlu
+firmware_10G="ps6m10gcic2l12octal8dio5v303" ##10 GBps - https://udtc-ot-firmware.web.cern.ch/?dir=v3-00/ps_8m_10g_cic2_l12octa_l8dio5_tlu
 runFpgaConfig = False ## it will run automatically if necessary
 ## command used to launch commands through Docker (podman)
 ## -v /home/thermal/suvankar/power_supply/:/home/thermal/suvankar/power_supply/
